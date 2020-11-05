32c9c55c2aa0785d04be4163ba5020ffc9460a9c
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2020-11-05 11:13:25 +0100

    Fixxed Errors
    Fixxed the input of the numbers and added operators

--- calculator.c
+++ calculator.c
@@ -2,30 +2,41 @@
 
 /**
 * Simple calculator
-*/
+/
 int main(){
+    float num1,num2;
+    float ans;
+    char op;
+    printf("\nEnter an operator (+,-,/,): \n");
+    scanf("%c",&op);
 
-	float num1;
-	int num2;
-	float ans;
-	char op;
-	printf("\nEnter an operator (+,-,/,*): \n");
-	scanf("%c",&op);
+    printf("\nEnter two numbers: ");
+    scanf("%f",&num1);
+    scanf("%f",&num2);
 
-	printf("\nEnter two numbers: ");
-	scanf("%f",&num1);
-	scanf("%f",&num2);
-	
-	switch(op){
-		case '+' : 
-			ans = num1 + num2;
-			break;
-		default:
-			printf("Enter a correct operator!");
-			return 1;
-	
-	}
-	printf("\n The result is as follows:\n");
-	printf("%f %c %f = %f",num1, op ,num2,ans);
-	return 0;
+    switch(op){
+        case '+' :
+            ans = num1 + num2;
+            break;
+        case '-' :
+            ans = num1 - num2;
+            break;
+        case '*' :
+            ans = num1 * num2;
+            break;
+        case '/' :
+            if(num2==0){
+                printf("Error divided by 0");
+                return 1;
+            }
+            ans = num1 / num2;
+            break;
+        default:
+            printf("Enter a correct operator!");
+            return 1;
+
+    }
+    printf("\n The result is as follows:\n");
+    printf("%f %c %f = %f",num1, op ,num2,ans);
+    return 0;
 }
